df32bf31d8408049191c83d63c16a8a292581c62
Student <Student@1A-D9-WINDOWS>
2025-02-22 15:48:21 +0000

    Add project files.

---
+++ DungeonTest/Encounters.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonTest
+{
+    internal class Encounters
+    {
+        //Encounter Generic
+
+
+        //Encounters
+        public static void FirstEncounter()
+        {
+            Console.WriteLine("You charge towards your captor, grabbing a rusty sword");
+            Console.WriteLine("He turns...");
+            Console.ReadKey();
+        }
+
+
+        //Encounter Tools
+        public static void Combat(bool random, string name, int power, int health)
+        {
+            string n = "";
+            int p = 0;
+            int h = 0;
+            if (random)
+            {
+
+            }
+            else
+            {
+                n = name;
+                p = power;
+                h = health;
+            }
+            while (h > 0) 
+            {
+                Console.WriteLine("-------------------------");
+                Console.WriteLine("|  (A)ttack    (D)efend  |");
+                Console.WriteLine("|  (R)un       (H)eal    |");
+                Console.WriteLine("-------------------------");
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ DungeonTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonTest
+{
+    internal class Program
+    {
+        public static Player currentPlayer = new Player();
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        static void Start() 
+        {
+            Console.WriteLine("Welcome to the Dungeon!");
+            Console.WriteLine("Name:");
+            currentPlayer.name = Console.ReadLine();
+            Console.WriteLine("You have woken up in the first room of the dungeon.");
+            if (currentPlayer.name == "")
+                Console.WriteLine("You can't even remeber your name");
+            else
+                Console.WriteLine("You can only remember that your name is "+currentPlayer.name);
+            Console.ReadKey();
+            Console.WriteLine("You grope around in the darkness and find a door. You go through.");
+            Console.WriteLine("You see your captor in the next room, facing away from you.");
+        }
+
+    }
+}
